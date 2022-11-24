3b59fe04c2c53a8fee6f90d4296513cedad63396
ramasubbaiah <ramasubbaiah@LAPTOP-8FTKGC45>
2022-11-24 16:27:22 +0530

    uhgf

---
+++ src/main/java/DEVOPSLAB/MH1A0570MJS5/App.java
@@ -0,0 +1,13 @@
+package DEVOPSLAB.MH1A0570MJS5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
